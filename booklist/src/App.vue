291fb1f324267d0e1969f3c9f54be02a4b1316d2
PappAdam <71632262+PappAdam@users.noreply.github.com>
2024-12-10 07:52:05 +0100

    booklist uploaded

---
+++ booklist/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Table from './components/Table.vue';
+import books from './bookdata';
+import Card from './components/Card.vue';
+
+// Reaktív változó létrehozása
+const isTableView = ref(false);
+</script>
+
+<template>
+  <button @click="isTableView = !isTableView" class="btn">
+    {{ isTableView ? 'Switch to Card View' : 'Switch to Table View' }}
+  </button>
+  <Table v-if="isTableView" :books="books" />
+  <Card v-else :books="books" />
+</template>
+
+<style scoped>
+  .btn{
+    background-color: rgb(114, 114, 250);
+    color: white;
+  }
+</style>
